620a5d55cfae8eefb88b3e5961bd52205fb39196
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2024-02-17 20:46:53 -0600

    feat(shopping-page): initializer section

--- src/02-component-pattern/pages/shopping.pages.tsx
+++ src/02-component-pattern/pages/shopping.pages.tsx
@@ -1,56 +1,33 @@
 import React from 'react';
 import {ProductButtons, ProductCard, ProductImage} from "../components";
-import {useProductCart} from "../hooks";
 
 import {Products} from "../data/products";
 
 import shoppingStyles from '../styles/shopping-page.module.css';
 import '../styles/custom-styles.css';
+const product = Products[0];
 
+return (
+  <div className={shoppingStyles.shopping}>
+    <main className={shoppingStyles.content}>
+      <header>
+        <h1>Shopping Pages</h1>
+      </header>
+      <hr/>
+      <article className={shoppingStyles.articles}>
+        <ProductCard
+          key={product.id}
+          className="card-dark"
+          product={product}>
+          <ProductCard.Image className="image-rounded"/>
+          <ProductCard.Title/>
+          <ProductCard.Buttons/>
+        </ProductCard>
+      </article>
+    </main>
+  </div>
+);
+}
 
-export const ShoppingPages = () => {
-  const {shoppingCart, onProductCountChange} = useProductCart();
-
-  return (
-    <div className={shoppingStyles.shopping}>
-      <main className={shoppingStyles.content}>
-        <header>
-          <h1>Shopping Pages</h1>
-        </header>
-        <hr/>
-        <article className={shoppingStyles.articles}>
-          {Products.map(product => (
-            <ProductCard
-              key={product.id}
-              className="card-dark"
-              onChange={onProductCountChange}
-              value={shoppingCart.get(product.id)?.count ?? 0}
-              product={product}>
-              <ProductCard.Image className="image-rounded"/>
-              <ProductCard.Title/>
-              <ProductCard.Buttons/>
-            </ProductCard>
-          ))}
-        </article>
-      </main>
-      <aside className={shoppingStyles.cart}>
-        <h4>Cart</h4>
-        {
-          Array.from(shoppingCart.entries()).map(([id, product]) => (
-            <ProductCard
-              key={id}
-              className="card-cart card-dark"
-              onChange={onProductCountChange}
-              value={product.count}
-              product={product}>
-              <ProductImage className="image-rounded"/>
-              <ProductButtons/>
-            </ProductCard>
-          ))
-        }
-      </aside>
-    </div>
-  );
-};
 
 export default ShoppingPages;
